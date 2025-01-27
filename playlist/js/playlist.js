494431da2de600c0e235e73c08785a3160ad1795
oscar <oscar.caasmontes@gmail.com>
2025-01-27 10:08:49 +0100

    feat: add getAllPlayLists return

--- playlist/js/playlist.js
+++ playlist/js/playlist.js
@@ -41,7 +41,9 @@ const musicCatalog = () => {
      * Gets all playlists in the catalog.
      * @returns {Playlist[]} The list of all playlists.
      */
-    const getAllPlaylists = () => {};
+    const getAllPlaylists = () => {
+      return playlists
+    };
   
     /**
      * Removes a playlist from the catalog.
